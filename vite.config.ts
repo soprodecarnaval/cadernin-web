f31e536442e63f28c2246a75ba912c5733f7f5fa
Gustavo Brunoro <git@hitnail.net>
2024-08-30 17:50:16 -0300

    fix base addr for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const config: UserConfig = {
+  plugins: [react()],
+}
+
+// @ts-ignore
+if (import.meta.env.MODE === "production") {
+  config.base = "https://soprodecarnaval.github.io/cadernin-web/",
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(config);
