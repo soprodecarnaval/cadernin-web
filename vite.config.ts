9c2489008e86c62694bb352f55a293fec28c0946
Gustavo Brunoro <git@hitnail.net>
2024-08-30 17:54:02 -0300

    specify base in deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig, UserConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const config: UserConfig = {
-  plugins: [react()],
-};
-
-// @ts-ignore
-if (import.meta.env.MODE === "production") {
-  config.base = "https://soprodecarnaval.github.io/cadernin-web/";
-}
-
 // https://vitejs.dev/config/
-export default defineConfig(config);
+export default defineConfig({
+  plugins: [react()],
+});
