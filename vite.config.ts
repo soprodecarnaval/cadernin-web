4a6bc7c1df18b0b64fe2d8d91b5248215570bbf5
Gustavo Brunoro <git@hitnail.net>
2024-08-30 17:50:36 -0300

    fix config syntax

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 
 const config: UserConfig = {
   plugins: [react()],
-}
+};
 
 // @ts-ignore
 if (import.meta.env.MODE === "production") {
-  config.base = "https://soprodecarnaval.github.io/cadernin-web/",
+  config.base = "https://soprodecarnaval.github.io/cadernin-web/";
 }
 
 // https://vitejs.dev/config/
